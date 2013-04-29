99c5d8fd3cf92ee05321c2bad8ec2eba992cb774
Haseeb Ahmad <engg.haseeb@gmail.com>
2013-04-29 16:51:13 +0500

    Code Commit

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'rails', '3.0.4'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3'
-
+gem 'aws-s3', :require => 'aws/s3'
 gem "paperclip", "~> 2.3"
 
 # Use unicorn as the web server
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,26 @@
+require 'paperclip'
+
 class User < ActiveRecord::Base    
   validates :name, :presence => true    
   validates :email, :presence => true
-  has_attached_file :pic, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  
+  #has_attached_file :pic, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   has_attached_file :attach
+
+  ACCESS_KEY_ID = 'AKIAJDPPPEEFNVBFDP7A'
+  SECRET_ACCESS_KEY = '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'
+  BUCKET = '10021testbucket'
+
+  has_attached_file :pic,
+    :storage => 's3',
+    :bucket => BUCKET,
+    :path => "uploads/:attachment/:id/:styles.:extension",
+    :styles => {
+      :medium => "300x300>",
+      :thumb => "100x100>" },
+    :s3_credentials => {
+      :access_key_id => ACCESS_KEY_ID,
+      :secret_access_key => SECRET_ACCESS_KEY },
+    :s3_permissions => 'public-read'
+
 end
